a3ca7549e96304a956d7df3aaafed5fbef4df263
NinputerWonder <ninputewonder@outlook.com>
2020-08-25 11:02:19 +0800

    4 use the equality instead of the field

--- src/test/java/org/wonder/MoneyExchangeTest.java
+++ src/test/java/org/wonder/MoneyExchangeTest.java
@@ -10,17 +10,14 @@ public class MoneyExchangeTest
     public void testMultiplication()
     {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
     public void testEquality()
     {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(5), five);
-        assertNotEquals(new Dollar(6), five);
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(6), new Dollar(5));
     }
 }
